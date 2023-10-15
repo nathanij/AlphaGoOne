5734fd7b88660bc2e5aae1f22ba08de2f44bf52f
Nathan James <nathanieljames@Nathans-MacBook-Pro-72.local>
2023-10-14 21:35:45 -0400

    oops

--- mcts/sim_driver.py
+++ mcts/sim_driver.py
@@ -1,4 +1,5 @@
 from game_state import GameState
+from tree.search_tree_header import SearchTreeHeader
 from networks.value_network import ValueNetwork
 from networks.policy_network import PolicyNetwork
 
@@ -8,4 +9,7 @@ class SimDriver:
         self.policy_network_ = PolicyNetwork(policy_network_path)
         self.value_network_ = ValueNetwork(value_network_path)
         self.state_ = GameState()
-        self.tree_ = 
+        self.tree_ = SearchTreeHeader()
+
+    def simulate(self):
+        
