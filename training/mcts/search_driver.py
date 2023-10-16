274951c1482936c81d9720e951eb4c3e41cf4bc1
Nathan James <nathanieljames@Nathans-MacBook-Pro-72.local>
2023-10-16 00:28:18 -0700

    Main structure, sim_driver work

--- training/mcts/search_driver.py
+++ training/mcts/search_driver.py
@@ -1,7 +1,14 @@
-from training.mcts import search_tree_header
+from mcts.search_tree_header import SearchTreeHeader
 
 
 class SearchDriver:
-    def __init__(self, tree: search_tree_header):
+    # Minimize instead of maximize if black is moving
+    def __init__(self, tree: SearchTreeHeader, search_limit: int, active_player: int):
         self.tree_ = tree
-        
+        self.active_player_ = active_player
+        self.iterations_ = 0
+        self.search_limit_ = search_limit
+
+    def finished(self):
+        return self.iterations_ >= self.search_limit_
+    
--- training/networks/value_network.py
+++ training/networks/value_network.py
@@ -1,6 +1,7 @@
 import pytorch
 
 class ValueNetwork:
+    # Network evaluates the probability of a white win from the given situation
     def __init__(self, weight_path: str):
         self.weight_path_ = weight_path
         self.model_ = pytorch.load(self.address_)  # TODO: correct method
